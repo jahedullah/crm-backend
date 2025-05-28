d75a12984864a7b4f1e367b0a59d14ce4200f26d
jahedul <jahedulislam6060@gmail.com>
2025-05-28 21:59:06 +0600

    create generic api response.

---
+++ src/main/java/com/relatiq/crm_backend/dto/ApiResponse.java
@@ -0,0 +1,14 @@
+package com.relatiq.crm_backend.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import java.time.ZonedDateTime;
+
+@Getter
+@Builder
+public class ApiResponse<T> {
+    private final ZonedDateTime timestamp;
+    private final int status;
+    private final String message;
+    private final T data;
+}
