7cee5b603ce6340c182bd78d041b7ca7fc16eaf3
jahedul <jahedulislam6060@gmail.com>
2025-05-28 20:20:17 +0600

    Initial push with project dir setup.

---
+++ src/test/java/com/relatiq/crm_backend/CrmBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relatiq.crm_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
